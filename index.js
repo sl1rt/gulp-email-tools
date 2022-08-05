1c165fbcfdb99e978c83e32446475844676309ee
unknown <sl1rtc@gmail.com>
2022-08-05 11:46:20 +0300

    Оптимизация кода, замена проверок на регулярные выражения

--- index.js
+++ index.js
@@ -19,204 +19,123 @@ module.exports = (options) => {
     }
 
     // Код плагина
-    let styles = [];
+    const styles = [];
+    const fileLines = file.contents.toString().split('\n');
+    let parseStyle = false;
+    let styleParseStart = 0;
 
-    function validateCSS() {
-      let unallowedSizeFormats = [
-        '%','pt','em','rem'
-      ];
-      let lines = file.contents.toString().split('\n');
+    function errorMessage(text, index, type) {
+      let errorLine = index+1;
+      if(type=='error')
+        console.log('\x1b[31m',`${file.path}:${errorLine} Ошибка`, "\x1b[37m", text)
+      else 
+        console.log("\x1b[33m",`${file.path}:${errorLine}`, "\x1b[37m", text)
+    } 
 
-      let errorMessage = (text, index, type) => {
-        let errorLine = index+1;
-
-        if(type=='error')
-          console.log('\x1b[31m',`${file.path}:${errorLine} Ошибка`, "\x1b[37m", text)
-        else 
-          console.log("\x1b[33m",`${file.path}:${errorLine}`, "\x1b[37m", text)
+    function validateCSS(item, index) {
+      if(item.includes('<style>')) { 
+        parseStyle = true; 
+        styleParseStart = index+1;
+        return; 
       } 
-
-      let parseStyle = false;
-
-      lines.forEach(function(item, index) {
-       
-        if(item.includes('<style>')) { parseStyle = true; return; } 
-        if(item.includes('</style>')) { parseStyle = false; return; }
- 
-        if(parseStyle) {
-          unallowedSizeFormats.forEach(function(sizeformat) {
-            if(item.toLowerCase().includes(sizeformat) && !item.toLowerCase().includes('text-size-adjust')) {
-              errorMessage(`${sizeformat} запрещен в письмах. Разрешенный формат задания размеров PX`, index);
-            }
-          });
-
-          if(item.match(/:not/ig)) {
-            errorMessage('Правило :not и превдоселекторы не поддерживаются', index , 'error');
-          }
-          
-          if(item.match(/\*/ig)) {
-            errorMessage('Правило * не поддерживается', index , 'error');
-          }
-        }
-      });
-    }
-
-    function validateHTML() {
-
-      let hasError = false;
-      let unallowedTags = [
-        'div','p','ul','li','ol','picture',
-        'h1','h2','h3','h4','h5','h6',
-      ];
-      let unallowedAttributes = [
-        'colspan','rowspan'
-      ];
-      let unallowedFormats = [
-        'svg','webp','avif'
-      ];
-      let singleTags = [
-        'img','meta','br','hr'
-      ];
-
-      let lines = file.contents.toString().split('\n');
-      let setErrorTrue = () => {
-        if(hasError == false) console.log(' ');
-        hasError = true;
+      if(item.includes('</style>')) { 
+        parseStyle = false; 
+        return;
       }
 
-      lines.forEach(function(item, index) {
-
-        let errorMessage = (text, type) => {
-          let errorLine = index+1;
-          if(type=='error')
-            console.log('\x1b[31m',`${file.path}:${errorLine} Ошибка`, "\x1b[37m", text)
-          else 
-            console.log("\x1b[33m",`${file.path}:${errorLine}`, "\x1b[37m", text)
-        } 
-
-        singleTags.forEach(function(tag) {
-          const itemRegexp = RegExp('<'+tag+'(.+?)>', 'ig');
-          let imgTag = item.match(itemRegexp);
-          if(imgTag) {
-            imgTag.forEach(function(tagInLine){
-              if(!tagInLine.match('.*/>')) {
-                setErrorTrue();
-                errorMessage(tag+ ' упущено закрытие тэга />','error');
-              } 
-            });
-          }
-        });
-        
-        if(item.match(/<(.*)/ig) && !item.match(/<(.*?)>/ig)) {
-          setErrorTrue();
-          errorMessage('Инлайнер не поддерживает перенос аттрибутов на новую строку','error');
+      if(parseStyle) {
+        if(item.toLowerCase().match(/:(.)*(pt|em|%|rem)(.)*;/ig) && !item.toLowerCase().includes('text-size-adjust')) {
+          errorMessage(`pt/%/em/rem запрещены в письмах. Рекомендуется использовать PX`, index);
         }
-
-        if(item.includes('<!DOCTYPE') && item != '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">') {
-          setErrorTrue();
-          errorMessage('Используйте корректный тип документа <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">');
+        if(item.toLowerCase().match(/:(.)*{/)) {
+          errorMessage(`Запрещено использовать псевдоселекторы и псевдоэлементы`, index, 'error');
         }
-        
-        unallowedAttributes.forEach(function(attr) {
-          if(item.toLowerCase().includes(attr)) {
-            setErrorTrue();
-            errorMessage(attr+ ' запрещено использовать в письмах');
-          }
-        });
-
-        unallowedTags.forEach(function(tag) {
-          if(item.toLowerCase().includes('<'+tag)) {
-            setErrorTrue();
-            errorMessage(`<${tag}></${tag}> запрещено использовать в письмах`);
-          }
-        });
-
-        unallowedFormats.forEach(function(format) {
-          if(item.toLowerCase().includes('.'+format)) {
-            setErrorTrue();
-            errorMessage(`${format} запрещен в письмах. Разрешенный формат изображений: JPG, PNG`);
-          }
-        });
-
-        let aTag = item.match(/<a(.*?)>|<a>/ig)
-        if(aTag) {
-          aTag.forEach(function(aItem) {
-            if(!aItem.toLowerCase().includes('target')) {
-              setErrorTrue();
-              errorMessage('<a> укажите target="_blank"');
-            }
-            if(!aItem.toLowerCase().includes('title')) {
-              setErrorTrue();
-              errorMessage('<a> укажите title=""');
-            }
-            if(!aItem.toLowerCase().includes('http')) {
-              setErrorTrue();
-              errorMessage('<a> укажите протокол ссылки https:// или http://');
-            }
-          })
+        if(item.toLowerCase().match(/.(.)*\[(.)*{/) ) {
+          errorMessage(`Селекторы по аттрибутам [] не поддерживаются инлайнером`, index, 'error');
         }
-
-        let imgTag = item.match(/<img>|<img(.*?)>/ig)
-        if(imgTag) {
-          imgTag.forEach(function(imgItem) {
-            if(!imgItem.toLowerCase().includes('alt')) {
-              setErrorTrue();
-              errorMessage('<img> укажите аттрибут ALT');
-            }
-            if(!imgItem.toLowerCase().includes('width')) {
-              setErrorTrue();
-              errorMessage('<img> укажите аттрибут WIDTH');
-            }
-            if(!imgItem.toLowerCase().includes('height')) {
-              setErrorTrue();
-              errorMessage('<img> укажите аттрибут HEIGHT');
-            }
-          });
+        if(item.match(/\*/ig)) {
+          errorMessage('Правило * не поддерживается', index , 'error');
         }
-
-        let tableTag = item.match(/<table>|<table(.*?)>/ig);
-        if(tableTag) {
-          tableTag.forEach(function (tableItem) {
-            if(!tableItem.toLowerCase().includes('cellpadding')) {
-              setErrorTrue();
-              errorMessage('TABLE должна иметь атрибут cellpadding');
-            }
-            if(!tableItem.toLowerCase().includes('cellspacing')) {
-              setErrorTrue();
-              errorMessage('TABLE должна иметь атрибут cellspacing');
-            }
-            if(!tableItem.toLowerCase().includes('width')) {
-              setErrorTrue();
-              errorMessage('TABLE должна иметь атрибут width');
-            }
-            if(!tableItem.toLowerCase().includes('border')) {
-              setErrorTrue();
-              errorMessage('TABLE должна иметь атрибут border="0"');
-            }
-            if(!tableItem.toLowerCase().includes('bgcolor')) {
-              setErrorTrue();
-              errorMessage('TABLE должна иметь атрибут bgcolor="#FFFFFF"');
-            }
-          });
-        }
-      });
-
-      if(hasError == true) console.log(' ');
+      }
     }
 
-    function collectStyles() {
-      let lines = file.contents.toString().split('\n');
-      let parseStyle = false;
-      let thisStyles = {};
-      let startStyleScope = false;
-      let stylesData = [];
-      let stylesName = [];
+    function validateHTML(item, index) {
+      let matched = [];
+      let testLine = item.toLowerCase();
 
-      lines.forEach(function(item, index) {
+      if(matched = testLine.match(/<(img|meta|br|hr)(.*?)(?<!\/)>/ig)) {
+        errorMessage(`${matched.join(', ')} упущено закрытие тэга />`, index, 'error');
+      }
+      
+      if(testLine.match(/<(.*)/ig) && !testLine.match(/<(.*?)>/ig)) {
+        errorMessage('Инлайнер не поддерживает перенос аттрибутов на новую строку', index,'error');
+      }
 
-        if(item.includes('<style>')) { parseStyle = true; return; } 
-        if(item.includes('</style>')) { parseStyle = false; return; }
+      if(testLine.includes('<!DOCTYPE') && testLine.trim() != '<!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">') {
+        errorMessage('Используйте корректный тип документа <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">', index);
+      }
+    
+      if(matched = testLine.match(/(colspan|rowspan)="(.*?)"/ig)) {
+        errorMessage(`${matched.join(', ')} запрещено использовать в письмах`, index);
+      }
 
+      if(matched = testLine.match(/<(div|p|ul|li|ol|picture|h1|h2|h3|h4|h5|h6)>/ig)) {
+        errorMessage(`${matched.join(', ')} запрещено использовать в письмах`, index);
+      }
+
+      if(matched = testLine.match(/\.(svg|webp|avif)/ig)) {
+        errorMessage(`Формат ${matched.join(', ')} запрещен в письмах. Разрешенный формат изображений: JPG, PNG`, index);
+      }
+
+      // Проверка ссылок
+      if(testLine.match(/<a((.(?!http))*|(.(?!target="_blank"))*|(.(?!title="\w+"))*)>/ig)) {
+        errorMessage('<a> для ссылок обязательно указание title, target="_blank" и протокола ссылок http/https', index);
+      }
+
+      // Проверка аттрибутов img
+      if(matched = testLine.match(/<img(.)*?>/ig)) {
+        matched.forEach((i) => {
+          if(i.match(/<img((.(?!alt="(.+)"))*)>/ig) ) {
+            errorMessage('<img> обязательно указывать alt', index);
+          }
+          if(i.match(/<img(.(?!width="\d+"))*>/ig) ) {
+            errorMessage('<img> обязательно указывать width', index);
+          }
+          if(i.match(/<img(.(?!height="\d+"))*>/ig) ) {
+            errorMessage('<img> обязательно указывать height', index);
+          }
+        });
+      }
+
+      // Проверка аттрибутов table
+      if(matched = testLine.match(/<table(.)*?>/ig)) {
+        matched.forEach((i) => {
+          if(i.match(/<table((.(?!cellpadding="(.+)"))*)>/ig) ) {
+            errorMessage('<table> обязательно указывать cellpadding', index);
+          }
+          if(i.match(/<table(.(?!cellspacing="(.+)"))*>/ig) ) {
+            errorMessage('<table> обязательно указывать cellspacing', index);
+          }
+          if(i.match(/<table(.(?!width="[\d%]+"))*>/ig) ) {
+            errorMessage('<table> обязательно указывать width', index);
+          }
+          if(i.match(/<table(.(?!border="(.+)"))*>/ig) ) {
+            errorMessage('<table> обязательно указывать border=""', index);
+          }
+          if(i.match(/<table(.(?!bgcolor="(.{7})"))*>/ig) ) {
+            errorMessage('<table> обязательно указывать bgcolor="#FFFFFF"', index);
+          }
+        });
+      }
+    }
+
+    let thisStyles = {};
+    let startStyleScope = false;
+    let stylesData = [];
+    let stylesName = [];
+    
+    function collectStyles(item, index) {
+      if(styleParseStart < index && parseStyle) { 
         if(!startStyleScope && !item.includes('{') && item.trim() != '') {
           stylesName.push(item.trim().substr(0, item.length));
         }
@@ -241,7 +160,7 @@ module.exports = (options) => {
         if (startStyleScope) {
           stylesData.push( item.toString().trim() );
         }
-      });
+      }
     }
 
     function getStylesForElement(values) {
@@ -256,68 +175,67 @@ module.exports = (options) => {
       return neededStyles.join('');
     }
 
-    function setStyles() {
-      let lines = file.contents.toString().split('\n');
-      let newLines = [];
-      lines.forEach((oneLine, lineIndex) => {
-        let openSearch = oneLine.trim().match(/<(.*?)>/ig);
+    let newLines = [];
+
+    function setStyles(oneLine, lineIndex) {
+
+      let openSearch = oneLine.trim().match(/<(.*?)>/ig);
         
-        if(openSearch) {
+      if(openSearch) {
 
-          openSearch.forEach((tagItem, index) => {
-            let tagClassesArray = [];
-            let getClassesRegexp = '';
-            let classesString = '';
+        openSearch.forEach((tagItem, index) => {
+          let tagClassesArray = [];
+          let getClassesRegexp = '';
+          let classesString = '';
 
-            let tagSearch = tagItem.match(/<([a-zA-z]+)/ig);
-            if(!tagSearch) return;
+          let tagSearch = tagItem.match(/<([a-zA-z]+)/ig);
+          if(!tagSearch) return;
 
-            let endOfTag = tagSearch[0].toString().length;
-            let tagName = tagSearch[0].toString().substr(1);
+          let endOfTag = tagSearch[0].toString().length;
+          let tagName = tagSearch[0].toString().substr(1);
 
-            tagClassesArray.push(tagName);
+          tagClassesArray.push(tagName);
 
-            if(getClassesRegexp = tagItem.match(/class=("|')(.+?)("|')/ig)) {
-              classesString = getClassesRegexp[0].toString();
-              classesString = classesString.replace(/(class|=|'|")/gi, '');
+          if(getClassesRegexp = tagItem.match(/class=("|')(.+?)("|')/ig)) {
+            classesString = getClassesRegexp[0].toString();
+            classesString = classesString.replace(/(class|=|'|")/gi, '');
 
-              classesString.split(' ').forEach(function(className) {
-                tagClassesArray.push('.'+className);
-              });
-            }
+            classesString.split(' ').forEach(function(className) {
+              tagClassesArray.push('.'+className);
+            });
+          }
 
-            let tagArray = tagItem.trim().split('');
+          let tagArray = tagItem.trim().split('');
 
-            let styles = getStylesForElement(tagClassesArray);
+          let styles = getStylesForElement(tagClassesArray);
 
-            if(styles != '') {
-              //вставляем стили
-              tagArray.splice(endOfTag, 0, ' ',`style="${styles}"`)
-            }
-            
-            let returnValue = tagArray.join('');
+          if(styles != '') {
+            //вставляем стили
+            tagArray.splice(endOfTag, 0, ' ',`style="${styles}"`)
+          }
+          
+          let returnValue = tagArray.join('');
 
-            oneLine = oneLine.replace(tagItem, returnValue);
-          });
+          oneLine = oneLine.replace(tagItem, returnValue);
+        });
 
-          newLines.push(oneLine.replace(/class=('|")(.*?)('|")/gi, ''));
-        } 
-        else
-        {
-          newLines.push(oneLine);
-        }
-
-      });
-
-      let newFile = newLines.join('\n').replace(/<style>((.|\s)*?)<\/style>/ig,'');
-
-      file.contents = Buffer.from(newFile);
+        newLines.push(oneLine.replace(/class=('|")(.*?)('|")/gi, ''));
+      } 
+      else
+      {
+        newLines.push(oneLine);
+      }
     }
+    
+    fileLines.forEach((item, index) => {
+      validateCSS(item, index);
+      validateHTML(item, index);
+      collectStyles(item, index);
+      setStyles(item, index);
+    });
 
-    validateCSS();
-    validateHTML();
-    collectStyles();
-    setStyles();
+    let newFile = newLines.join('\n').replace(/<style>((.|\s)*?)<\/style>/ig,'');
+        file.contents = Buffer.from(newFile);
 
     this.push(file);
     cb();
